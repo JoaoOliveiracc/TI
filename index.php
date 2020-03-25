1e8f74b86656f6b1026f888fa5ccf4e4ee4934ec
João Monteiro <ccjoao_monteiro@outlook.com>
2020-03-25 10:54:19 -0300

    Métodos de listagem

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
     require_once("config.php");
 
-    $user = new Usuario();
+    // Crrega um usuario
+    // $user = new Usuario();
+    // $user->loadById(3);
 
-    $user->loadById(3);
+    // echo $user;
 
-    echo $user;
+    // Carrega lista de usuários
+    // $lista = Usuario::getList();
+
+    // echo json_encode($lista);
+
+    // Carrega uma lista de usuários buscando pelo login
+    // $search = Usuario::search("jo");
+
+    // echo json_encode($search);
+
+    // Carrega um usuário usando o login e a senha
+    $usuario = new Usuario();
+
+    $usuario->login("josé", "1234567890");
+
+    echo $usuario;
 
 ?>
