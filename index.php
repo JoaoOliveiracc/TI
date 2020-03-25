2770288d2a05870ef52de45d7d9d0d8034b5b2bf
João Monteiro <ccjoao_monteiro@outlook.com>
2020-03-25 12:56:03 -0300

    Insert e Update

--- index.php
+++ index.php
@@ -19,9 +19,27 @@
     // echo json_encode($search);
 
     // Carrega um usuário usando o login e a senha
+    // $usuario = new Usuario();
+
+    // $usuario->login("josé", "1234567890");
+
+    // echo $usuario;
+
+    // Criando um novo usuário
+    // $aluno = new Usuario("aluno", "@lun0");
+
+    // $aluno->setDesLogin("aluno");
+    // $aluno->setDesSenha("@alun0");
+
+    // $aluno->insert();
+
+    // echo $aluno;
+
     $usuario = new Usuario();
 
-    $usuario->login("josé", "1234567890");
+    $usuario->loadById(4);
+
+    $usuario->update("professor", "pr0f3ss0r");
 
     echo $usuario;
 
